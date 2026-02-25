e6103f33497df21b7e003347b6de0c440822e311
abdelmounaimoulaid <oulaidabdelmonaim2@gmail.com>
2026-02-25 12:13:38 +0000

    fix: set main to dist/index.js, add prestart build step, create root js entry point for Hostinger

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file acts as a proxy for the actual dist output.
+// Certain hosting environments (like Hostinger) default to looking for `index.js` at the root.
+
+require('./dist/index.js');
