ce42ccbd23ec6833fe1c348fe07a956cc9e1327b
abdelmounaimoulaid <oulaidabdelmonaim2@gmail.com>
2026-02-25 13:01:20 +0000

    chore: add universal crash logger to root index.js to identify passenger boot issues

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // This file acts as a proxy for the actual dist output.
 // Certain hosting environments (like Hostinger) default to looking for `index.js` at the root.
 
-const app = require('./dist/index.js').default || require('./dist/index.js').app;
+const fs = require('fs');
+function logError(err) {
+    fs.writeFileSync('./hostinger-error.log', new Date().toISOString() + ' ' + (err.stack || err) + '\n', { flag: 'a' });
+}
+process.on('uncaughtException', logError);
+process.on('unhandledRejection', logError);
+
+let app;
+try {
+    app = require('./dist/index.js').default || require('./dist/index.js').app;
+} catch (err) {
+    logError(err);
+}
 module.exports = app;
